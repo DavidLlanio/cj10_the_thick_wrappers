da388977839ccc02660d02c33bd61d746b990851
DavidLlanio <llaniodavid@gmail.com>
2023-09-01 19:40:10 -0400

    Images can be uploaded and stored in static folder

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-from nicegui import ui
+import os
+import re
+
+from nicegui import events, ui
+
+
+# GUI helper functions
+def handle_image_upload(img: events.UploadEventArguments, cover=False):
+    """Handle user image to encrypt.
+
+    This function will take the image that was uploaded by the user
+    and put it in the static file folder of the repository as
+    user_image.(file extension of original image)
+    or
+    cover_image.(file extension of original image)
+
+    param img: object that has uploaded file
+    """
+    # Get the binary of the tempfile object
+    content = img.content.read()
+    # Get the extension using regex and check that it is valid
+    acceptable_extensions = ["jpg", "png", "jpeg"]
+    file_extension = re.search(r"\.([a-zA-Z0-9]+)$", img.name).group(1)
+    if file_extension not in acceptable_extensions:
+        ui.notify("Not an acceptable file type!")
+        return
+    # Save the image locally if the file extension is valid
+    if cover:
+        file_path = os.path.join("static", f"cover_image.{file_extension}")
+    else:
+        file_path = os.path.join("static", f"user_image.{file_extension}")
+    with open(file_path, "wb") as f:
+        f.write(content)
+
 
 # Title of the project
 ui.label("The Thick Wrappers Steganography Project")
@@ -26,7 +59,7 @@ with ui.card().bind_visibility_from(dropdown_encrypt_or_decrypt, "value", value=
             with ui.column():
                 ui.label("Enter Cover Image:")
             with ui.column():
-                ui.upload(on_upload=lambda e: ui.notify(f"Uploaded {e.name}"))
+                ui.upload(auto_upload=True, on_upload=lambda e: handle_image_upload(e, cover=True))
         with ui.row():
             ui.button("Encrypt", on_click=lambda: ui.notify("Encrypted!"))
     # User input needed if image message type is chosen
@@ -36,13 +69,13 @@ with ui.card().bind_visibility_from(dropdown_encrypt_or_decrypt, "value", value=
             with ui.column():
                 ui.label("Enter Image to Encrypt:")
             with ui.column():
-                ui.upload(on_upload=lambda e: ui.notify(f"Uploaded {e.name}"))
+                ui.upload(auto_upload=True, on_upload=handle_image_upload)
         # Prompt the user for the image they want to encrypt a message into
         with ui.row():
             with ui.column():
                 ui.label("Enter Cover Image:")
             with ui.column():
-                ui.upload(on_upload=lambda e: ui.notify(f"Uploaded {e.name}"))
+                ui.upload(auto_upload=True, on_upload=lambda e: handle_image_upload(e, cover=True))
         with ui.row():
             ui.button("Encrypt", on_click=lambda: ui.notify("Encrypted!"))
 
@@ -54,7 +87,7 @@ with ui.card().bind_visibility_from(dropdown_encrypt_or_decrypt, "value", value=
             with ui.column():
                 ui.label("Enter Image to Decrypt:")
             with ui.column():
-                ui.upload(on_upload=lambda e: ui.notify(f"Uploaded {e.name}"))
+                ui.upload(auto_upload=True, on_upload=lambda e: handle_image_upload(e, cover=True))
         with ui.row():
             ui.button("Decrypt", on_click=lambda: ui.notify("Decrypted!"))
 
