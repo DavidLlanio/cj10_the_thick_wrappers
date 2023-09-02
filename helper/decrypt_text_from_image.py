78472ddc3ccdcb10f376d69d642f8875a92da9d9
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-09-02 16:29:30 -0400

    pixel to hex func finished

--- helper/decrypt_text_from_image.py
+++ helper/decrypt_text_from_image.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 def bin_to_ascii(binary):
     ascii = int(f"{binary}", 2)
     byte_number = ascii.bit_length() + 7 // 8
@@ -6,10 +8,22 @@ def bin_to_ascii(binary):
     return ascii_text
 
 # example
-print(bin_to_ascii(11000010110001001100011))
+print(bin_to_ascii(11000010110001001100011)) # "abc"
 
 
 def decrypt(image):
-    """Sample function"""
-    pass
+    img = Image.open(image)
+    pixels = img.load() 
+    print(pixels)
+    width, height = img.size
+    def pixel_to_hex(x, y):
+        if img.mode == 'RGBA':
+            r, g, b, a = pixels[x, y]
+            return r, g
+        else:
+            r, g, b = pixels[x, y]
+            return r, g
+    return pixel_to_hex
+    
 
+print(decrypt("/Users/maxencegilloteaux/Desktop/secret_image1_unscrambledcopy.png")(200, 50))
