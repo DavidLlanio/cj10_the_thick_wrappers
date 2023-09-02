4e940398ce4366902c6013c0db6aabf8b745815e
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-09-02 01:40:54 -0400

    Created function that take binary in and spits out the ascii version

--- helper/decrypt_text_from_image.py
+++ helper/decrypt_text_from_image.py
@@ -1,3 +1,15 @@
+def bin_to_ascii(binary):
+    ascii = int(f"{binary}", 2)
+    byte_number = ascii.bit_length() + 7 // 8
+    binary_array = ascii.to_bytes(byte_number, "big")
+    ascii_text = binary_array.decode()
+    return ascii_text
+
+# example
+print(bin_to_ascii(11000010110001001100011))
+
+
 def decrypt(image):
     """Sample function"""
     pass
+
