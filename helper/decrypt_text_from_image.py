3db5ca074ae6ca45e098360d266b15c6a647d722
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-09-02 17:08:25 -0400

    Function to convert pixel to binary

--- helper/decrypt_text_from_image.py
+++ helper/decrypt_text_from_image.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 def bin_to_ascii(binary):
-    ascii = int(f"{binary}", 2)
+    ascii = int(f"{(binary)}", 2)
     byte_number = ascii.bit_length() + 7 // 8
     binary_array = ascii.to_bytes(byte_number, "big")
     ascii_text = binary_array.decode()
@@ -9,21 +9,35 @@ def bin_to_ascii(binary):
 
 # example
 print(bin_to_ascii(11000010110001001100011)) # "abc"
-
+print(bin_to_ascii("0101010001101111011100100110111101101110011101000110111100100000010000110110000101101110011000010110010001100001")) # Toronto Canada
 
 def decrypt(image):
     img = Image.open(image)
     pixels = img.load() 
     print(pixels)
     width, height = img.size
-    def pixel_to_hex(x, y):
+    pixel_list = []
+
+    def pixel_to_bin(x, y):
         if img.mode == 'RGBA':
             r, g, b, a = pixels[x, y]
-            return r, g
+            r, g, b = int(str(bin(r))[2:]), int(str(bin(g))[2:]), int(str(bin(b))[2:])
+            return r, g, b
+        
         else:
             r, g, b = pixels[x, y]
-            return r, g
-    return pixel_to_hex
+            r, g, b = int(str(bin(r))[2:]), int(str(bin(g))[2:]), int(str(bin(b))[2:])
+            return r, g, b
+    
+    def decrypt_each_pixel():
+        for y in range(height):
+            for x in range(width):
+                rgb_val = pixel_to_bin(x, y)
+                pixel_list.append(rgb_val)
+
+    decrypt_each_pixel()
+
+    return pixel_list
     
 
-print(decrypt("/Users/maxencegilloteaux/Desktop/secret_image1_unscrambledcopy.png")(200, 50))
+# print(decrypt("/Users/maxencegilloteaux/Desktop/secret_image1_unscrambledcopy.png"))
