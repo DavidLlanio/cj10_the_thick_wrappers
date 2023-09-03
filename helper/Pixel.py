d8963a98b1d05070e97300db49fb54d2920f6e38
jaavv <mjaavv@gmail.com>
2023-09-03 13:24:48 -0600

    added typing to methods and override toString

--- helper/Pixel.py
+++ helper/Pixel.py
@@ -1,31 +1,32 @@
 class Pixel:
-    def __init__(self, red, green, blue):
+
+    def __init__(self, red: int, green: int, blue: int):
         self.__red = red
         self.__green = green
         self.__blue = blue
 
-    def get_red(self):
+    def get_red(self) -> int:
         return self.__red
 
-    def get_green(self):
+    def get_green(self) -> int:
         return self.__green
 
-    def get_blue(self):
+    def get_blue(self) -> int:
         return self.__blue
 
-    def get_red_bin(self):
+    def get_red_bin(self) -> str:
         return bin(self.__red).replace("0b", "").zfill(8)
 
-    def get_green_bin(self):
+    def get_green_bin(self) -> str:
         return bin(self.__green).replace("0b", "").zfill(8)
 
-    def get_blue_bin(self):
+    def get_blue_bin(self) -> str:
         return bin(self.__blue).replace("0b", "").zfill(8)
 
-    def get_rgb(self):
+    def get_rgb(self) -> tuple[int, int, int]:
         return self.get_red(), self.get_green(), self.get_blue()
 
-    def get_rgb_bin(self):
+    def get_rgb_bin(self) -> tuple[str, str, str]:
         return self.get_red_bin(), self.get_green_bin(), self.get_blue_bin()
 
     def set_red(self, value):
@@ -54,3 +55,9 @@ class Pixel:
         self.set_red(values[0])
         self.set_green(values[1])
         self.set_blue(values[2])
+
+    def __str__(self) -> str:
+        return f'RED={self.__red},GREEN={self.__green},BLUE={self.__blue}'
+
+    def __repr__(self) -> str:
+        return f'RGB({self.__red}, {self.__green}, {self.__blue})'
