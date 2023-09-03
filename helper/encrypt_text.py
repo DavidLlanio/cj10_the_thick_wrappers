4a5cebf94a2939ec63304982359e6cc527c8a72a
standard-affect <86736798+standard-affect@users.noreply.github.com>
2023-09-03 16:19:25 -0400

    Add file to test encryption/decryption, and sample image

--- helper/encrypt_text.py
+++ helper/encrypt_text.py
@@ -2,48 +2,69 @@ from PIL import Image
 
 ASCII_MAX = 127
 # Added as message end
-PADDING = list(map(ord, ",,,.."))
+END_TEXT = ",,,.."
+END_BYTES = list(map(ord, END_TEXT))
 
 
 # Convert string to bytes, removing non-ASCII characters
 def strip_non_ascii(text: str) -> list[int]:
-    """Convert string to bytes, removing non-ASCII characters"""
+    """
+    Convert string to character values, removing non-ASCII characters
+
+    param text: string to convert
+    """
     result = []
     for char in text:
-        if ((byte := ord(char)) <= ASCII_MAX):
+        if (byte := ord(char)) <= ASCII_MAX:
             result.append(byte)
     return result
 
 
-def encrypt_text(text: str, image_path: str) -> Image.Image:
-    """Encode a text string in randomly selected coordinates of an image"""
-    with Image.open(image_path) as image:
-        # Convert to ASCII and add padding indicating message end
-        bytes = strip_non_ascii(text)
-        n = len(bytes)
-        rows, cols = image.size
+def encrypt_text(text: str, image: Image.Image) -> Image.Image | None:
+    """
+    Encode a text string in randomly selected coordinates of an image
 
-        extent = rows * cols
-        if n > extent:
-            raise ValueError(f"Cannot encode {n} characters in image of {extent} pixels")
-        # Pixel coordinates, going left and down
-        targets = [(c // cols, c % cols) for c in range(n)]
+    :param text Message to encrypt.
+    :param image Pillow `Image` object in which `text` will be encrypted. Must have only three color planes.
 
-        # Alter 3 LSBs for each target pixel
-        bit_length = 3
-        modulus = 2 ** bit_length
+    This function encrypts a string in an image. Non-ASCII characters are
+    stripped from the string. For each character, a pixel of the image is selected,
+    going left and down from the top left corner.
+    For each pixel, the 7 bits of the corresponding ASCII code are encoded in
+    the three least significant bits
+    of each color plane (red, green, blue).
+    Blue receives only one bit. The modified image with the encoded message is
+    returned.
 
-        for target, byte in zip(targets, bytes):
-            pixel: list[int] = image.getpixel(target)
-            pixel = list(pixel)
+    If the input text contains more characters than the image has pixels,
+    encryption is impossible, so `None` is returned.
+    """
+    # Convert to ASCII and add padding indicating message end
+    bytes = strip_non_ascii(text) + END_BYTES
+    n = len(bytes)
+    rows, cols = image.size
 
-            for i, plane in enumerate(pixel):
-                # Clear 3 LSB
-                plane >>= bit_length
-                plane <<= bit_length
-                pixel[i] = plane + byte % modulus
-                byte >>= bit_length
-            image.putpixel(target, tuple(pixel))
+    extent = rows * cols
+    # Alert caller if too many bytes to encode
+    if n > extent:
+        return None
+    # Pixel coordinates, going left and down
+    targets = [(c // cols, c % cols) for c in range(n)]
 
-        # Save original values?
-        return image
+    # Alter 3 LSBs for each target pixel
+    bit_length = 3
+    modulus = 2 ** bit_length
+
+    for target, byte in zip(targets, bytes):
+        pixel: list[int] = image.getpixel(target)
+        pixel = list(pixel)
+
+        for i, plane in enumerate(pixel):
+            # Clear 3 LSB
+            plane >>= bit_length
+            plane <<= bit_length
+            pixel[i] = plane + byte % modulus
+            byte >>= bit_length
+        image.putpixel(target, tuple(pixel))
+
+    return image
---
+++ test-encryption.py
@@ -0,0 +1,62 @@
+# Demonstrates encyption on a sample image
+import string
+from random import choices
+
+from PIL import Image
+
+from helper import encrypt_text
+
+chars = string.ascii_letters + string.digits + string.punctuation
+
+
+def random_message(length: int) -> str:
+    """Generate random characters of a certain length
+
+    param length: Number of characters to select.
+    """
+    return "".join(choices(chars, k=length))
+
+
+def test_message(message: str, image: Image.Image) -> None:
+    """Confirm a message is correctly encoded into an image.
+
+    param message: String to encode.
+    param image: Image in which to encode
+
+    This function checks that `encrypt_text` returns a valid
+    result if called with the given message and image. It
+    then decodes the message and confirms that it is identical to the original
+    text.
+    """
+    end_length = len(encrypt_text.END_TEXT)
+    result = encrypt_text.encrypt_text(message, image.copy())
+    assert result
+
+    cols = result.size[1]
+    decrypt = []
+    modulus = 8
+
+    # Decode least significant bits in each pixel
+    for pixel in [(n // cols, n % cols) for n in range(len(message) + end_length)]:
+        value = 0
+        for i, plane in enumerate(result.getpixel(pixel)):
+            bits = plane % modulus
+            bits <<= 3 * i
+            value += bits
+        decrypt.append(chr(value))
+    # Combine characters and remove end-of-message padding
+    decrypt = "".join(decrypt)[: len(decrypt) - end_length]
+    assert decrypt == message
+
+
+# Test different message sizes
+# From https://www.rawpixel.com/image/7514064/photo-image-public-domain-galaxy-space
+image = Image.open("nebula.jpeg")
+for length in (0, 5, 100, 5000, 15000):
+    message = random_message(length)
+    test_message(message, image)
+
+# Maximum length message
+test_message("a" * 9995, image.crop((0, 0, 100, 100)))
+# One char too many
+assert encrypt_text.encrypt_text("a" * 9996, image.crop((0, 0, 100, 100))) is None
