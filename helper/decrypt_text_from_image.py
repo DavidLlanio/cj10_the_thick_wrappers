5bc909318956469373867c2094b4fd0d3a17ab02
Maxence <122239025+maxence-glt@users.noreply.github.com>
2023-09-02 23:06:07 -0400

    Modified all_pixels_to_binary funciton to allow for Pillow objects to pass through

--- helper/decrypt_text_from_image.py
+++ helper/decrypt_text_from_image.py
@@ -1,30 +1,17 @@
-from PIL import Image
-
-image = "" #TODO add image input here
+image = ""  # TODO add image input here
 
 
-
-
-
-# Translates an inputted image's pixels to binary
 def all_pixels_to_binary(image):
-    img = Image.open(image)
-    pixels = img.load() 
-    print(pixels)
-    width, height = img.size
+    """Translates an inputted image's pixels to binary"""
+    pixels = image.load()
+    width, height = image.size
     pixellist = []
 
     def pixel_to_bin(x, y):
-        if img.mode == 'RGBA':
-            r, g, b, a = pixels[x, y]
-            r, g, b = int(str(bin(r))[2:]), int(str(bin(g))[2:]), int(str(bin(b))[2:])
-            return r, g, b
-        
-        else:
-            r, g, b = pixels[x, y]
-            r, g, b = int(str(bin(r))[2:]), int(str(bin(g))[2:]), int(str(bin(b))[2:])
-            return r, g, b
-    
+        r, g, b = pixels[x, y]
+        r, g, b = int(str(bin(r))[2:]), int(str(bin(g))[2:]), int(str(bin(b))[2:])
+        return r, g, b
+
     # Iterates through all of the picture's pixels, left to right then down
     def pixel_list():
         for y in range(height):
@@ -36,11 +23,8 @@ def all_pixels_to_binary(image):
     return pixellist
 
 
-
-
-
-# Iterates and checks every binary RGB triplet, scanning over the last 3 LSB
 def binary_decoder(image):
+    """Iterates and checks every binary RGB triplet, scanning over the last 3 LSB"""
     RGB_binary_list = all_pixels_to_binary(image)
     for pixel in RGB_binary_list:
         output = ""
@@ -48,22 +32,13 @@ def binary_decoder(image):
             y = str(y)
             output += y[-3:]
         print(bin_to_ascii(output))
-    
 
 
-
-
-# Traslates inputted binary to ASCII
 def bin_to_ascii(binary):
+    """Traslates inputted binary to ASCII"""
     ascii = int(f"{(binary)}", 2)
-    byte_number = ascii.bit_length() + 7 // 8
+    byte_number = (ascii.bit_length() + 7) // 8
     binary_array = ascii.to_bytes(byte_number, "big")
     ascii_text = binary_array.decode()
     return ascii_text
-# example: print(bin_to_ascii(11000010110001001100011)) # "abc"
-
-
-
-
-
-print(binary_decoder(image))
+print(bin_to_ascii("011000010110001001100011")) # "abc"
