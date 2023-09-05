e0454f0f2895f1988c76c0315fa1e1e8767d2772
jaavv <mjaavv@gmail.com>
2023-09-05 13:54:57 -0600

    StegaImage refactor

--- helper/StegaImage.py
+++ helper/StegaImage.py
@@ -1,65 +1,7 @@
 from enum import Enum
-from typing import Any
 from PIL import Image
 
 import numpy as np
-from numpy import ndarray, dtype
-
-from Pixel import Pixel
-
-
-class StegaImage:
-    def __init__(self, image: Image):
-        self.image = image
-        self.image_as_array = np.asarray(image)
-        self.format = image.format
-        self.size = image.size
-        self.mode = image.mode
-
-    def get_pixel_binary(self, coordinates: tuple[int, int]) -> ndarray[Any, dtype[Any]]:
-        decimal_value = self.image_as_array[coordinates]
-        px_bin = Pixel(decimal_value[0], decimal_value[1], decimal_value[2])
-        return np.array(px_bin.get_rgb_bin())
-
-    def get_pixel_lsb(self, coordinates) -> ndarray[Any, dtype[Any]]:
-        px_bin = self.get_pixel_binary(coordinates)
-        lsb_value = {}
-        for index, binary in enumerate(px_bin):
-            lsb = parse_sb(binary, SignificantBit.LSB)
-            lsb_value[index] = lsb
-        return np.array((lsb_value[0], lsb_value[1], lsb_value[2]))
-
-    def get_pixel_msb(self, coordinates):
-        px_bin = self.get_pixel_binary(coordinates)
-        msb_value = []
-        for index, binary in enumerate(px_bin):
-            msb = parse_sb(binary, SignificantBit.MSB)
-            msb_value.append(msb)
-        return np.asarray(msb_value)
-
-    def get_image_bits(self):
-        height, width, mode = self.image_as_array.shape
-        grid = np.empty(shape=(height, width), dtype='O')
-        for y in range(height):
-            for x in range(width):
-                grid[y, x] = self.get_pixel_binary((y, x))
-        return grid
-
-    def get_image_lsb(self):
-        height, width, mode = self.image_as_array.shape
-        grid = np.empty(shape=(height, width), dtype='O')
-        for y in range(height):
-            for x in range(width):
-                grid[y, x] = self.get_pixel_lsb((y, x))
-        return grid
-
-    def get_image_msb(self):
-        height, width, mode = self.image_as_array.shape
-        grid = np.empty(shape=(height, width), dtype='O')
-        for y in range(height):
-            for x in range(width):
-                grid[y, x] = self.get_pixel_msb((y, x))
-        return grid
 
 
 class SignificantBit(Enum):
@@ -67,9 +9,25 @@ class SignificantBit(Enum):
     LSB = 2
 
 
-def parse_sb(binary: str, sb: SignificantBit) -> str:
-    match sb:
-        case SignificantBit.MSB:
-            return binary[:4]
-        case SignificantBit.LSB:
-            return binary[4:]
+class StegaImage:
+    """ Pillow.Image delegate wrapper """
+
+    def __init__(self, image: Image.Image):
+        self.image = image
+        self._image_as_array = np.asarray(image)
+
+    def _update_image(self) -> None:
+        """ Convert array back to Image object """
+        self.image = Image.fromarray(self._image_as_array)
+
+    def reset_lsb(self) -> None:
+        """ Logical shift each pixel 4 bits to the right and back """
+        bits = 4
+        (self._image_as_array >> bits) << bits
+        self._update_image()
+
+    def reset_msb(self) -> None:
+        """ Logical shift each pixel 4 bits to the right """
+        bits = 4
+        self._image_as_array >> bits
+        self._update_image()
