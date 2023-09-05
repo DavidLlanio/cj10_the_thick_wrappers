5fed358cdea1e72318ca4d99e9b10191aeedd5a3
jaavv <mjaavv@gmail.com>
2023-09-05 16:02:49 -0600

    Steganographizer refactor to static method

--- helper/Steganographizer.py
+++ helper/Steganographizer.py
@@ -6,26 +6,22 @@ from helper.StegaImage import StegaImage
 
 class Steganographizer:
 
-    _stega_image: Image.Image = None
-
-    def __init__(self, cover: Image, secret: Image):
-        self._cover = StegaImage(cover)
-        self._secret = StegaImage(secret)
-
-    def encrypt_image(self) -> None:
+    @staticmethod
+    def encrypt_image(cover: Image, secret: Image) -> Image:
         """
         Apply image steganography by resetting the cover image's least significant 4 bits,
-        take the secret image's most significant 4 bits and add both values together
+        take the secret image's most significant 4 bits and add both numpy arrays together.
+        Sum of arrays is converted back into Pillow Image and returned.
         """
+        s_cover = StegaImage(cover)
+        s_secret = StegaImage(secret)
         print("Encrypting Image...")
-        self._cover.reset_lsb()
-        self._secret.take_msb()
-        stega_image_array = np.add(self._cover.get_image_array(), self._secret.get_image_array())
-        self._stega_image = Image.fromarray(stega_image_array)
+        s_cover.reset_lsb()
+        s_secret.take_msb()
+        stega_image_array = np.add(s_cover.get_image_array(), s_secret.get_image_array())
         print("Complete!")
+        return Image.fromarray(stega_image_array)
 
-    def save_image(self, destination: str) -> None:
-        try:
-            self._stega_image.save(destination)
-        except AttributeError:
-            print("No steganography image file found.")
+    @staticmethod
+    def decrypt_image(image: Image) -> Image:
+        pass
