2c3e4dbd1d9b57a5e47a481209b11a6fa79d4bb9
jaavv <mjaavv@gmail.com>
2023-09-07 15:02:30 -0600

    shift_image_bits_asarray function added, exif function fix, resize_image fuction tweaks

--- helper/utility.py
+++ helper/utility.py
@@ -1,22 +1,9 @@
-from enum import Enum
-
-from PIL import Image
 import numpy as np
+from PIL import Image
 from PIL.Image import Exif
 
-from helper.apl_info import APLInfo, APLExif
-
-
-class ResizeMode(Enum):
-    DEFAULT = 0
-    SHRINK_TO_SCALE = 1
-
-
-class Sizing(Enum):
-    SMALLER = 0
-    WIDER = 1
-    TALLER = 2
-    BIGGER = 3
+from helper import EXIF_MAKE, ExifData, ResizeMode, Sizing, \
+    STARTING_X, STARTING_Y, TEAM_MEMBERS, DESCRIPTION, Direction, SOFTWARE_TITLE
 
 
 def get_pixels_from_image():
@@ -29,76 +16,76 @@ def clear_least_significant_bits(bits):
     pass
 
 
-def clear_least_significant_bits_asarray(image: Image.Image) -> np.ndarray:
+def shift_image_bits_asarray(image: Image.Image, direction: Direction, bit_amount: int) -> np.asarray:
     """
-    Reset image least significant bit values.
-    1101 1011 -> 0000 1101 -> 1101 0000
+    Convert image to numpy array and perform bitwise operation
     :param image: Image object
-    :return: Return RGB values as numpy array
+    :param direction: Left bit shit or Right bit shift
+    :param bit_amount: Amount of bits to shift
+    :return: Image data as array
     """
-    bits = 4
-    image_as_array = np.asarray(image)
-    return (image_as_array >> bits) << bits
+    image_asarray = np.asarray(image)
+    match direction:
+        case Direction.LEFT:
+            return np.left_shift(image_asarray, bit_amount)
+        case Direction.RIGHT:
+            return np.right_shift(image_asarray, bit_amount)
 
 
-def msb_to_lsb_asarray(image: Image.Image) -> np.ndarray:
+def embed_apl_exif(image_exif: Exif, data: tuple[int, int]) -> Exif:
     """
-    Shift image most significant bit to least significant bit.
-    1101 1011 -> 0000 1101
-    :param image: Image object
-    :return: Return RGB values as numpy array
-    """
-    bits = 4
-    image_as_array = np.asarray(image)
-    return image_as_array >> bits
-
-
-def embed_exif(image_exif: Image.Image, data: APLInfo) -> Exif:
-    """
-    Update image EXIF image data
-    :param image_exif: Image object metadata is extracted from
+    Implement A Pixel Life metadata
+    :param image_exif: Image metadata
     :param data: New metadata to embed
-    :return: Return updated image EXIF type
+    :return: Updated image metadata
     """
-    exif = image_exif.getexif()
-    exif[APLExif.MAKE.value] = data.make
-    exif[APLExif.MODEL.value] = data.model
-    exif[APLExif.ARTIST.value] = data.artist
-    exif[APLExif.SOFTWARE.value] = data.software
-    exif[APLExif.DESCRIPTION.value] = data.description
-    return exif
+    image_exif[ExifData.MAKE.value] = EXIF_MAKE
+    image_exif[ExifData.MODEL.value] = exif_model_builder(data)
+    image_exif[ExifData.ARTIST.value] = TEAM_MEMBERS
+    image_exif[ExifData.SOFTWARE.value] = SOFTWARE_TITLE
+    image_exif[ExifData.DESCRIPTION.value] = DESCRIPTION
+    return image_exif
+
+
+def exif_model_builder(size: tuple[int, int]) -> str:
+    """
+    Build a secret code for decryption function to analyze
+    :param size: Secret image dimensions
+    :return: Encrypted message
+    """
+    width, height = size
+    return f"A{width}P{height}L"
 
 
 def resize_image(image: Image.Image, max_dimension: tuple[int, int], resize_mode=ResizeMode.DEFAULT) -> Image.Image:
     """
-    Resize image object.\n
+    Resize image object\n
     DEFAULT - Crop any exceeding dimensions\n
     SHRINK_TO_SCALE - Shrink image to scale of maximum dimensions while keeping aspect ratio\n
     :param image: Image object
     :param max_dimension: Dimensions image cannot exceed
     :param resize_mode: Resize mode
-    :return: Resized Image object. Returns the same image if smaller than max dimensions.
+    :return: Resized Image object. Returns the same image if smaller than max dimensions
     """
     current_image_width, current_image_height = image.size
     max_width, max_height = max_dimension
     image_copy = image.copy()
     sizing_mode = Sizing.SMALLER
-    starting_point = 0
-    if (current_image_width > max_width) and (current_image_height < max_height):
-        sizing_mode = Sizing.WIDER
-    if (current_image_height > max_height) and (current_image_width < max_width):
-        sizing_mode = Sizing.TALLER
     if (current_image_height > max_height) and (current_image_width > max_width):
         sizing_mode = Sizing.BIGGER
+    if (current_image_height > max_height) and (current_image_width < max_width):
+        sizing_mode = Sizing.TALLER
+    if (current_image_width > max_width) and (current_image_height < max_height):
+        sizing_mode = Sizing.WIDER
     match resize_mode:
         case ResizeMode.DEFAULT:
             match sizing_mode:
                 case Sizing.BIGGER:
-                    image_copy = image_copy.crop((starting_point, starting_point, max_width, max_height))
+                    image_copy = image_copy.crop((STARTING_X, STARTING_Y, max_width, max_height))
                 case Sizing.TALLER:
-                    image_copy.crop((starting_point, starting_point, current_image_width, max_height))
+                    image_copy.crop((STARTING_X, STARTING_Y, current_image_width, max_height))
                 case Sizing.WIDER:
-                    image_copy.crop((starting_point, starting_point, max_width, current_image_height))
+                    image_copy.crop((STARTING_X, STARTING_Y, max_width, current_image_height))
         case ResizeMode.SHRINK_TO_SCALE:
             match sizing_mode:
                 case Sizing.BIGGER:
