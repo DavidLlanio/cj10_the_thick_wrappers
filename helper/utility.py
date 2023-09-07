77b0cf59b30ed063e9b2c562a3674d9d70005a72
jaavv <mjaavv@gmail.com>
2023-09-06 20:01:57 -0600

    resize_image implementation

--- helper/utility.py
+++ helper/utility.py
@@ -44,27 +44,44 @@ def embed_exif(image_exif: Image.Image, data: APLInfo) -> Exif:
     return exif
 
 
-def resize_image(image: Image.Image, max_dimension: tuple[int, int], mode: ResizeMode) -> Image.Image:
+def resize_image(image: Image.Image, max_dimension: tuple[int, int], mode=ResizeMode.DEFAULT) -> Image.Image:
+    """
+    Resize image object.\n
+    DEFAULT - Crop any exceeding dimensions\n
+    SHRINK_TO_SCALE - Shrink image to scale of maximum dimensions while keeping aspect ratio\n
+    :param image: Image object
+    :param max_dimension: Dimensions image cannot exceed
+    :param mode: Resize mode
+    :return: Resized Image object
+    """
     current_image_width, current_image_height = image.size
     max_width, max_height = max_dimension
     image_copy = image.copy()
     match mode:
         case ResizeMode.DEFAULT:
-            pass
+            # if wider
+            if (current_image_width > max_width) and (current_image_height < max_height):
+                image_copy.crop((0, 0, max_width, current_image_height))
+            # if taller
+            if (current_image_height > max_height) and (current_image_width < max_width):
+                image_copy.crop((0, 0, current_image_width, max_height))
+            # if bigger
+            if (current_image_height > max_height) and (current_image_width > max_width):
+                image_copy = image_copy.crop((0, 0, max_width, max_height))
         case ResizeMode.SHRINK_TO_SCALE:
             # if wider
             if (current_image_width > max_width) and (current_image_height < max_height):
                 width_ratio = current_image_width / max_width
                 new_height = int(current_image_height // width_ratio)
-                image_copy.thumbnail((max_width, new_height), Image.ANTIALIAS)
+                image_copy.thumbnail((max_width, new_height), Image.LANCZOS)
             # if taller
             if (current_image_height > max_height) and (current_image_width < max_width):
                 height_ratio = current_image_height / max_height
                 new_width = int(current_image_width // height_ratio)
-                image_copy.thumbnail((new_width, max_height), Image.ANTIALIAS)
+                image_copy.thumbnail((new_width, max_height), Image.LANCZOS)
             # if bigger
             if (current_image_height > max_height) and (current_image_width > max_width):
-                image_copy.thumbnail((max_width, max_height), Image.ANTIALIAS)
+                image_copy.thumbnail((max_width, max_height), Image.LANCZOS)
     return image_copy
 
 
